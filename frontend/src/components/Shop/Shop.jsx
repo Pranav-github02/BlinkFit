4b15141a1e473428f2fe1144d0bd90cc21d0ec5f
Pranav Jindal <71919420+Pranav-github02@users.noreply.github.com>
2023-04-21 17:02:07 +0530

    added shop

--- frontend/src/components/Shop/Shop.jsx
+++ frontend/src/components/Shop/Shop.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import './shop.css'
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Hello Shop</h1>
+    <div className="shop">
+      <div className="banner page-title">
+        <div className="container">
+          <div className="title-heading">
+            <h1>SHOP</h1>
+          </div>
+          <div className="sub-catg">
+            <div className="content">
+              <ul>
+                <li>
+                  <a href=""><i class="fa-thin fa-shirt"></i></a>
+                  <h2>clothing</h2>
+                </li>
+                <li>
+                  <a href=""><i class="fa-thin fa-boxing-glove"></i></a>
+                  <h2>accessories</h2>
+                </li>
+                <li>
+                  <a href=""><i class="fa-thin fa-boot"></i></a>
+                  <h2>footwear</h2>
+                </li>
+                <li>
+                  <a href=""><i class="fa-thin fa-dumbbell"></i></a>
+                  <h2>equipment</h2>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
